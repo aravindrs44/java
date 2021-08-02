0f024bbf7da340100e025c4489c21e5934ea0c95
aravindrs44 <aravindrs@hotmail.com>
2021-08-02 15:21:15 -0700

    initial commit

---
+++ java_oop/java_oop/ZooKeeperPartOne/src/Mammal.java
@@ -0,0 +1,21 @@
+
+public class Mammal {
+	public int energyLevel;
+	
+	public Mammal() {
+		this.energyLevel = 100;
+	}
+	
+	public Mammal(int energyLevel) {
+		this.energyLevel = energyLevel;
+	}
+	
+	public void setEnergy(int energy) {
+		this.energyLevel = energy;
+	}
+	
+	public int displayEnergy() {
+		System.out.println(energyLevel);
+		return energyLevel;
+	}
+}
